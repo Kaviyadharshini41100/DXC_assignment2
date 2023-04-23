c02fc4b606b5f23015b70ac52e85a43a8bed364c
S <kaviyadharshini.s@dxc.com>
2023-04-23 19:26:20 +0530

    Game_Program

---
+++ Game_Program/Game_Program/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Game_Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+                Console.WriteLine("Enter no of Robbers:");
+                int i = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter The Robber with whom Sam will start shooting:");
+                int j = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter the gap between the robbers:");
+                int G = Convert.ToInt32(Console.ReadLine());
+                int m = j; //m=current robber
+                int n = 1; //n=Shoots completed
+                while (i > 0)
+                {
+                    Console.WriteLine("Target = " + m + ", " + n + " shots completed");
+                    n++;
+                    i--;
+                    m = (m + G) % (i + 1);
+                    if (m == 0)
+                    {
+                        m = i;
+                    }
+                }
+                Console.WriteLine(n+ " shots taken.");
+            }
+        }
+
+ }
